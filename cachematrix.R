a25c60cc14fc7001ded11760607208e0b4324908
lethezhou <lethezhou@gmail.com>
2014-06-21 14:15:46 +0800

    add some annotations

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creat a special "matrix" containing a list of 4 functions
+## for solving and caching the Inverse matrix of the origin
 
-## Write a short comment describing this function
+## This function creat a matrix with 4 functions: set, get, setInverse and get Inverse.
+## The actual solving and caching procession is in next function "cacheSolve"
 
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
@@ -18,7 +19,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function get an "matrix with 4 functions", and try to use the cached inverse matrix
+## If cached data does not exist, it will solve it and cache it in 'x'.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
